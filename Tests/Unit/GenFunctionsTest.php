98e3e99db75513c63df5f08611ad3857fe20012f
addshore <addshorewiki@gmail.com>
2014-02-01 01:45:01 +0100

    Write more tests and add @covers tags

--- Tests/Unit/GenFunctionsTest.php
+++ Tests/Unit/GenFunctionsTest.php
@@ -17,9 +17,44 @@ class GenFunctionsTest extends \PHPUnit_Framework_TestCase {
 
 	/**
 	 * @dataProvider provide_iin_array
+	 * @covers ::iin_array
 	 */
 	public function test_iin_array( $needle, $haystack, $expected ) {
 		$this->assertEquals( $expected, iin_array( $needle, $haystack ) );
+	}
+
+	public function provide_strtoupper_safe() {
+		return array(
+			array( 'foo', 'FOO' ),
+			array( 'FOO', 'FOO' ),
+			array( array( 'foo' ), array( 'FOO' ) ),
+			array( array( array( 'foo' ) ), array( array( 'FOO' ) ) ),
+			array( array( array( 'foo' ), array( 'boo' ), 'F' ), array( array( 'FOO' ), array( 'BOO' ), 'F' ) ),
+		);
+	}
+
+	/**
+	 * @dataProvider provide_strtoupper_safe
+	 * @covers ::strtoupper_safe
+	 */
+	public function test_strtoupper_safe( $input, $expected ) {
+		$this->assertSame( $expected, strtoupper_safe( $input ) );
+	}
+
+	public function provide_in_string() {
+		return array(
+			array( 'B', 'aBc', false , true ),
+			array( 'b', 'aBc', false , false ),
+			array( 'b', 'aBc', true , true ),
+		);
+	}
+
+	/**
+	 * @dataProvider provide_in_string
+	 * @covers ::in_string
+	 */
+	public function test_in_string( $needle, $haystack, $insensitive, $expected ) {
+		$this->assertEquals( $expected, in_string( $needle, $haystack, $insensitive ) );
 	}
 
 } 
