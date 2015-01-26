a1c3ff51dcbe8678a2c3d940c00713035e2f7b5d
Calebrw <calebrw@gmail.com>
2015-01-25 21:23:30 -0600

    Typo edits to config.inc.php
    weather > whether
    Cleanup spacing.

--- config.inc.php
+++ config.inc.php
@@ -1,11 +1,11 @@
 <?php
-    //This file is the default configuration file for Peachy.  Please do not modify it.  Please create a file called config.local.inc.php with the variables you want altered.
+    //This file is the default configuration file for Peachy. Please do not modify it. Please create a file called config.local.inc.php with the variables you want altered.
     
-    //Controls weather or not the peachy tag is displayed at the end of edit summaries.
+    //Controls whether or not the peachy tag is displayed at the end of edit summaries.
     $pgNotag = false;
     $pgTag = " ([[en:WP:PEACHY|Peachy ".PEACHYVERSION."]])";
     
-    //For debugging purposes.  This generates enourmous amounts of data.  Switch off if your space is limited.
+    //For debugging purposes. This generates enourmous amounts of data. Switch off if your space is limited.
     $pgLogGetCommunicationData = true;
     $pgLogPostCommunicationData = true;
     $pgLogCommunicationData = true;
@@ -15,7 +15,7 @@
     //Bot output
     $pgDisplayPostOutData = true;
     $pgDisplayGetOutData = true;
-    $pgDisplayPechoVerbose = false; //Major security risk if set to true.  Switch to true at your own risk.
+    $pgDisplayPechoVerbose = false; //Major security risk if set to true. Switch to true at your own risk.
     $pgDisplayPechoNormal = true;
     $pgDisplayPechoNotice = true;
     $pgDisplayPechoWarn = true;
@@ -23,13 +23,13 @@
     $pgDisplayPechoFatal = true;
     $pgWebOutput = false; //Switch to true if you are using a webserver to output your data instead of commandline.
     
-    //This controls bot checks, before doing an action.  This change will affect every bot using it.  Enabling it drops the edit rate.
+    //This controls bot checks, before doing an action. This change will affect every bot using it. Enabling it drops the edit rate.
     $pgDisablechecks = false;
     
-    //Sometimes a site certificate can't be verified causing the connection to be terminated.  Set this to false, if this is the case.  Do so at your own risk.
+    //Sometimes a site certificate can't be verified causing the connection to be terminated. Set this to false, if this is the case. Do so at your own risk.
     $pgVerifyssl = true;
     
-    //Disable automatic updates.  You will need to download updates manually then, until it is re-enabled again.
+    //Disable automatic updates. You will need to download updates manually then, until it is re-enabled again.
     $pgDisableUpdates = false;
     $pgExperimentalupdates = false;
     
@@ -50,7 +50,7 @@
     $pgUsername = null;
     $pgPassphrase = null; //Passphrase to decrypt key file for authentication or password to authenticate with to server
     $pgPrikey = null;     //File path to the private key file.
-    $pgProtocol = 2;      //SSH protocol to use.  1=SSH1 or 2=SSH2.
+    $pgProtocol = 2;      //SSH protocol to use. 1=SSH1 or 2=SSH2.
     $pgTimeout = 10;
     
     //Import local settings if available
